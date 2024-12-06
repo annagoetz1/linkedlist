b42457b1efa88ee57360c21e13352fe5e8214aaa
annagoetz1 <annalgoetz1@gmail.com>
2024-12-06 15:45:08 -0500

    assign new list and append values

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import LinkedList from './LinkedList.js';
+
+const list = new LinkedList();
+
+// Adding example data
+list.append(10);
+list.append(20);
+list.prepend(5);
+
+// Displaying the linked list
+list.display(); // Output: 5 -> 10 -> 20
